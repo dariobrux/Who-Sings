2876260f1cdacfea024dd144b0b5d5bd8a65c4dc
Dario Bruzzese <dariobrux@gmail.com>
2020-12-10 19:19:30 +0100

    Avoid to come back to the splash screen

--- app/src/main/java/com/dariobrux/whosings/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/dariobrux/whosings/ui/splash/SplashFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.animation.OvershootInterpolator
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
 import androidx.navigation.fragment.NavHostFragment
 import com.dariobrux.whosings.R
 import com.dariobrux.whosings.databinding.FragmentSplashBinding
@@ -37,10 +38,6 @@ class SplashFragment : Fragment() {
         requireActivity().toMainActivity()?.setStatusBarColor(R.color.red_400)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-
-    }
-
     override fun onResume() {
         super.onResume()
         with(binding!!.imgSplash) {
@@ -48,7 +45,11 @@ class SplashFragment : Fragment() {
             scaleY = 0.2f
             alpha = 0f
             animate().scaleX(1f).scaleY(1f).alpha(1f).setDuration(300).setInterpolator(OvershootInterpolator()).withEndAction {
-                NavHostFragment.findNavController(requireParentFragment()).navigate(R.id.action_splashFragment_to_loginFragment)
+                NavHostFragment.findNavController(requireParentFragment()).navigate(
+                    R.id.action_splashFragment_to_loginFragment,
+                    null,
+                    NavOptions.Builder().setPopUpTo(R.id.splashFragment, true).build()
+                )
             }.start()
         }
     }
